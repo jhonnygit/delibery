da8e513d0b77b6a161dc0eb923649a5f4c7b6ded
jhonnygit <jhonprograma@gmail.com>
2023-10-10 10:09:49 -0400

    assets and color,images

--- src/screens/SplashScreen.js
+++ src/screens/SplashScreen.js
@@ -1,13 +1,22 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { View,Text,StyleSheet } from "react-native";
+import { Colors,Images } from "../contants";
 
 const SplashScreen=()=>{
     return(
         <View style={styles.container}>
-            <StatusBar barStyle="light-content"/>
-            <Image />
-            <Text></Text>
+            <StatusBar 
+                barStyle="light-content"
+                backgroundColor={Colors.DEFAULT_GREEN}
+                translucent
+            />
+            <Image 
+                source={Images.PLATE}
+                resizeMode="contain"
+                style={styles.image}
+            />
+            <Text style={styles.titleText}>Delivery</Text>
         </View>
     );
 };
@@ -17,7 +26,16 @@ const styles=StyleSheet.create({
         flex:1,
         justifyContent:'center',
         alignItems:"center"
+    },
+    image:{
+        height:150,
+        width:150,
+    },
+    titleText:{
+        color:Colors.DEFAULT_WHITE,
+        fontSize:32,
     }
+
 });
 
 export default SplashScreen;
